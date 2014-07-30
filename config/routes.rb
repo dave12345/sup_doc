4c54057a72b3f3663a504ce79e4c80f89547f2ee
Dav-o <dav-o@david-upjohns-computer.local>
2014-07-30 14:23:30 -0700

    Devise for Patient. Change root path to patient dash in patient model.

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,7 @@
 Rails.application.routes.draw do
-  get 'patients/index'
 
-  get 'patients/new'
 
-  get 'patients/create'
-
-  get 'patients/show'
-
-  get 'patients/edit'
-
-  get 'patients/update'
-
-  get 'patients/destroy'
-
+  root 'patients#patient_dash'
   devise_for :patients
   
   get 'patient_dash'           => 'temp_pages#patient_dash'
@@ -31,7 +20,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'temp_pages#home'
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
