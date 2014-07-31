159e34789730a0996cd31c97f0b23312303cfc71
Dav-o <dav-o@david-upjohns-computer.local>
2014-07-31 11:36:36 -0700

    Make create method for health_form. Make redirect to Webchat for patient on submit. Create _form.

--- app/controllers/health_forms_controller.rb
+++ app/controllers/health_forms_controller.rb
@@ -10,8 +10,20 @@ class HealthFormsController < ApplicationController
   end
 
   def create
+    @health_form = HealthForm.new(health_form_params)
+    if @health_form.save
+      redirect_to patient_webchat_path
+    else
+      render :new
+    end
   end
 
   def destroy
   end
+
+  private
+
+    def health_form_params
+      params.require(:health_form).permit(:first_name, :last_name, :symptoms, :patient_id)
+    end
 end
