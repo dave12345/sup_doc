f724014f6288912085b43ab63de990cee8038310
Dav-o <dav-o@david-upjohns-computer.local>
2014-07-31 18:53:51 -0700

    Install CarrierWave and add Avatar string column to Doctor_Profiles and Patient_Profiles tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140801005918) do
+ActiveRecord::Schema.define(version: 20140801015159) do
 
   create_table "appointments", force: true do |t|
     t.datetime "datetime"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20140801005918) do
     t.string   "doctor_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar"
   end
 
   create_table "doctors", force: true do |t|
@@ -82,6 +83,7 @@ ActiveRecord::Schema.define(version: 20140801005918) do
     t.integer  "patient_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar"
   end
 
   create_table "patients", force: true do |t|
